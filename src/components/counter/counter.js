48b0de65379ac30ccf3221cbffa140ecb265d063
Your Name <you@example.com>
2019-11-25 17:25:38 +0200

    final commit

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,5 +1,26 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Button, CardActions } from "@material-ui/core";
 import './counter.css';
+
+const useStyles = makeStyles({
+    gridContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: "30px",
+    },
+    gridItem: {
+        padding: "1.0rem",
+        borderRadius: "1rem",
+        boxShadow: "-14px 20px 32px -10px rgba(0,0,0,0.75)",
+        display: "flex",
+        flexDirection: "row",
+    },
+    cardMedia: {
+        height: "35vh",
+    },
+});
 
 const Counter = () => {
     const [counter, setCounter] = useState(0);
@@ -16,21 +37,51 @@ const Counter = () => {
         setCounter(counter > 0 ? counter-1 : 0)
     };
 
-    let test =
-        counter === 0 ? "grey"
-            : counter % 10 === 0 ? "orange"
-            : counter % 2 === 0 ? "red"
-            : "blue"
+    let color = "grey";
 
+    if (counter === 0) {
+        color = "grey";
+    } else {
+        if (counter === 100) reset();
+        if (counter % 2 === 0) color = "red";
+        if (counter % 2 === 1) color = "black";
+    }
+
+    const classes = useStyles();
     return (
-        <div>
-            <div className={test + " displayCounter"}><p>{counter}</p></div>
-            <div className="buttonContainer">
-                <button className="buttons addOne" onClick={ add }><p>Add one more</p></button>
-                <button className="buttons removeOne" onClick={ remove }><p>Remove one more</p></button>
-                <button className="buttons reset" onClick={ reset }><p>Reset</p></button>
+        <main className='circle-container'>
+            <div className={`circle ${color}`}>
+                {counter}
             </div>
-        </div>
+            <Grid container className={classes.gridContainer} spacing={2}>
+                <Grid item xs={3} className={classes.gridItem}>
+                    <CardActions>
+                        <Button
+                            onClick={ add }
+                            variant="contained"
+                            color="secondary">
+                            Add one more
+                        </Button>
+                    </CardActions>
+                    <CardActions>
+                        <Button
+                            onClick={ remove }
+                            variant="contained"
+                            color="secondary">
+                            Remove one more
+                        </Button>
+                    </CardActions>
+                    <CardActions>
+                        <Button
+                            onClick={ reset }
+                            variant="contained"
+                            color="secondary">
+                            Reset
+                        </Button>
+                    </CardActions>
+                </Grid>
+            </Grid>
+        </main>
     );
 }
 
